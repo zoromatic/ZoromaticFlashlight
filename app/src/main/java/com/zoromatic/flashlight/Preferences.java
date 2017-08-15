17f2381f7f2c872faadda9b3f781a25f9b21098c
zoromatic <zmatic@carnegietechnologies.com>
2017-08-16 00:40:34 +0200

    Initial commit

---
+++ app/src/main/java/com/zoromatic/flashlight/Preferences.java
@@ -0,0 +1,101 @@
+package com.zoromatic.flashlight;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+class Preferences {
+    static final String PREF_NAME = "com.zoromatic.flashlight.Preferences";
+    public static final String PREF_SETTINGS = "settings_";
+    static final String PREF_THEME = "theme_";
+    static final String PREF_COLOR_SCHEME = "colorscheme_";
+    static final String PREF_ORIENTATION = "orientation_";
+    static final String PREF_KEEPACTIVE = "keepactive_";
+    static final String PREF_TURNONONOPEN = "turnononopen_";
+    static final String PREF_KEEPSTROBEFREQ = "keepstrobefreq_";
+    private static final String PREF_STROBEFREQ = "strobefreq_";
+    static final String PREF_LANGUAGE_OPTIONS = "languageoptions_";
+
+    private static SharedPreferences getPreferences(Context context) {
+        return context.getSharedPreferences(PREF_NAME, 0);
+    }
+
+    public static String getTheme(Context context) {
+        return getPreferences(context).getString(
+                PREF_THEME + 0, "light"); // default: light
+    }
+
+    static int getColorScheme(Context context) {
+        return getPreferences(context).getInt(
+                PREF_COLOR_SCHEME + 0, 9); // default: cyan
+    }
+
+    static String getOrientation(Context context) {
+        return getPreferences(context).getString(
+                PREF_ORIENTATION + 0, "sens"); // default: sensor
+    }
+
+    static boolean getTurnOnOnOpen(Context context) {
+        return getPreferences(context)
+                .getBoolean(PREF_TURNONONOPEN + 0, false); // default: do not turn on
+    }
+
+    static boolean getKeepStrobeFrequency(Context context) {
+        return getPreferences(context)
+                .getBoolean(PREF_KEEPSTROBEFREQ + 0, false); // default: do not keep strobe frequency
+    }
+
+    static int getStrobeFrequency(Context context) {
+        return getPreferences(context)
+                .getInt(PREF_STROBEFREQ + 0, 0); // default: 0
+    }
+
+    static boolean getKeepActive(Context context) {
+        return getPreferences(context)
+                .getBoolean(PREF_KEEPACTIVE + 0, true); // default: keep active
+    }
+
+    static String getLanguageOptions(Context context) {
+        return getPreferences(context)
+                .getString(PREF_LANGUAGE_OPTIONS + 0, "");
+    }
+
+    static void setTheme(Context context, String value) {
+        getPreferences(context).edit()
+                .putString(PREF_THEME + 0, value).commit();
+    }
+
+    static void setColorScheme(Context context, int value) {
+        getPreferences(context).edit()
+                .putInt(PREF_COLOR_SCHEME + 0, value).commit();
+    }
+
+    static void setOrientation(Context context, String value) {
+        getPreferences(context).edit()
+                .putString(PREF_ORIENTATION + 0, value).commit();
+    }
+
+    static void setTurnOnOnOpen(Context context, boolean value) {
+        getPreferences(context).edit()
+                .putBoolean(PREF_TURNONONOPEN + 0, value).commit();
+    }
+
+    static void setKeepStrobeFrequency(Context context, boolean value) {
+        getPreferences(context).edit()
+                .putBoolean(PREF_KEEPSTROBEFREQ + 0, value).commit();
+    }
+
+    static void setStrobeFrequency(Context context, int value) {
+        getPreferences(context).edit()
+                .putInt(PREF_STROBEFREQ + 0, value).commit();
+    }
+
+    static void setKeepActive(Context context, boolean value) {
+        getPreferences(context).edit()
+                .putBoolean(PREF_KEEPACTIVE + 0, value).commit();
+    }
+
+    static void setLanguageOptions(Context context, String value) {
+        getPreferences(context).edit()
+                .putString(PREF_LANGUAGE_OPTIONS + 0, value).commit();
+    }
+}
---
+++ app/src/main/java/com/zoromatic/flashlight/SetPermissionsActivity.java
@@ -0,0 +1,167 @@
+package com.zoromatic.flashlight;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import com.alertdialogpro.AlertDialogPro;
+
+public class SetPermissionsActivity extends AppCompatActivity {
+    public static final String PERMISSIONS_TYPE = "PERMISSIONS_TYPE";
+    public static final int PERMISSIONS_REQUEST_CAMERA = 1;
+    private int mPermissionType = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_permissions);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            mPermissionType = extras.getInt(PERMISSIONS_TYPE);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Check permissions and open request if not granted
+            if (mPermissionType == PERMISSIONS_REQUEST_CAMERA) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                        new Thread() {
+                            @Override
+                            public void run() {
+                                SetPermissionsActivity.this.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        ExplanationDialogFragment question = new ExplanationDialogFragment();
+                                        question.setPermissionType(mPermissionType);
+                                        question.show(getSupportFragmentManager(), "ExplanationDialogFragment");
+                                    }
+                                });
+                            }
+                        }.start();
+                    } else {
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.CAMERA},
+                                PERMISSIONS_REQUEST_CAMERA);
+
+                        // PERMISSIONS_REQUEST_CAMERA is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                } else {
+                    initializeActivity();
+                }
+            }
+        } else {
+            initializeActivity();
+        }
+    }
+
+    private void initializeActivity() {
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    private void closeActivity() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                    initializeActivity();
+                } else {
+                    // permission denied
+                    closeActivity();
+                }
+
+                break;
+            }
+            default: {
+                closeActivity();
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public int getPermissionType() {
+        return mPermissionType;
+    }
+
+    public void setPermissionType(int mPermissionGroup) {
+        this.mPermissionType = mPermissionGroup;
+    }
+
+
+    public static class ExplanationDialogFragment extends DialogFragment {
+        Context mContext;
+        private int mPermissionType = -1;
+
+        public ExplanationDialogFragment() {
+            mContext = getActivity();
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            mContext = getActivity();
+            String theme = Preferences.getTheme(getContext());
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext,
+                    theme.compareToIgnoreCase("light") == 0 ? R.style.Theme_AlertDialogPro_Material_Light : R.style.Theme_AlertDialogPro_Material);
+
+            if (mPermissionType == PERMISSIONS_REQUEST_CAMERA) {
+                builder
+                        .setTitle(mContext.getResources().getString(R.string.permission_needed_camera))
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+
+                                ActivityCompat.requestPermissions(getActivity(),
+                                        new String[]{Manifest.permission.CAMERA},
+                                        PERMISSIONS_REQUEST_CAMERA);
+                            }
+                        })
+                        .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                                ((SetPermissionsActivity) mContext).finish();
+                            }
+                        });
+            }
+
+            return builder.create();
+        }
+
+        public int getPermissionType() {
+            return mPermissionType;
+        }
+
+        public void setPermissionType(int mPermissionType) {
+            this.mPermissionType = mPermissionType;
+        }
+    }
+}
